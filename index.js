06e767b5e8be47090e5ca48950c019390ebc985c
Taylor Osborn <osborn.taylor@gmail.com>
2020-01-14 10:49:51 -0800

    deleted all modules

--- index.js
+++ index.js
@@ -1,18 +1,69 @@
-const inquirer = require("inquirer");
+  
 const axios = require("axios");
-const electron = require("electron-html-to");
-const fs = require('fs')
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const convertFactory = require("electron-html-to");
+const electron = require("electron");
+const writeFileAsync = util.promisify(fs.writeFile);
+const readFileAsync = util.promisify(fs.readFile);
 
-const generateHTML = require("./generateHTML");
 
-const questions = [
 
-];
+const generateHTML = require("./generateHTML")
 
-function writeToFile(fileName, data) {
+
+
+
+
+
+
+function promptUser() {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your GitHub username?"
+      },
+      {
+        type: "list",
+        name: "color",
+        message: "What's your favorite color?",
+        choices: [
+          "green",
+          "blue",
+          "pink",
+          "red"
+        ]
+      },   
+    ]);
+  }
+
+  promptUser()
+  .then(function(data) {
+   const queryUrl = `https://api.github.com/users/${data.name}`;
+   const starsUrl = `https://api.github.com/users/${data.name}/starred`;
+    // console.log(queryUrl)
+    const color = data.color;
+    axios.get(queryUrl).then(function(res){
+      //console.log(res)
+
+        axios.get(starsUrl).then(function(stars){
+
+          //console.log(stars) 
+          const html = generateHTML({stars, color, res });
+          writeFileAsync("index.html", html);
+          
+        
+            });
+          });
+        });
+     
+  
+  
+  
  
-}
+  
 
-function init() {
+  
 
-init();}
